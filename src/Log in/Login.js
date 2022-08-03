daa9df442c54398bdecd17447193e20fc5ed1932
oshadaera68 <mindartlk010@gmail.com>
2022-08-03 23:54:32 +0530

    finished login form

--- src/Log in/Login.js
+++ src/Log in/Login.js
@@ -2,20 +2,28 @@ import './Login.css'
 import './normalize.css'
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 function LogIn(){
     return( 
     <div className="fill__container">
         <div className="login__cover">
             <div className='title__container'>
-            <Typography variant="h4" fontFamily="ubuntu" gutterBottom component="div">
-                Log In
+            <Typography variant="h4" fontFamily="comfortaa" gutterBottom component="div">
+                Log In Form
             </Typography>
             </div>
             <div className='form__container'>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="outlined-basic" type="password" label="Outlined" variant="outlined" />
+            <TextField id="standard-basic" className='text1' size='small' label="User Name" variant="standard" />
+            <TextField id="standard-basic" className='text2' type='password' label="Password" variant="standard" />
             </div>
-            <div className='btn__container'></div>     
+            <div className='btn__container'>
+                <Button variant="contained" className='btn' fontFamily="Ubuntu">Log in</Button>
+            </div>
+            <div className='userAcc_container'>
+            <Typography variant="h8" gutterBottom fontFamily="Comfortaa" color="white" component="div">
+                Create new user account? <a href='#'>click here</a>
+            </Typography>
+            </div>     
         </div>  
     </div>
     );
