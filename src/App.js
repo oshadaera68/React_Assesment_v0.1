aba83d1ea57071f78a183889b58de149f3a2a069
oshadaera68 <mindartlk010@gmail.com>
2022-08-06 21:36:01 +0530

    updated content with background image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import LogIn from "./Log in/Login"
 function App() {
   return (
     <div className="App">
-      {/* <h1>hello</h1> */}
       <LogIn />
+      
     </div>
   );
 }
--- src/Log in/Login.js
+++ src/Log in/Login.js
@@ -3,25 +3,26 @@ import './normalize.css'
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import React from 'react';
 function LogIn(){
     return( 
     <div className="fill__container">
         <div className="login__cover">
             <div className='title__container'>
-            <Typography variant="h4" fontFamily="comfortaa" gutterBottom component="div">
-                Log In Form
-            </Typography>
+                <Typography variant="h4" fontFamily="comfortaa" gutterBottom component="div">
+                    Log In Form
+                </Typography>
             </div>
             <div className='form__container'>
             <TextField id="standard-basic" className='text1' size='small' label="User Name" variant="standard" />
             <TextField id="standard-basic" className='text2' type='password' label="Password" variant="standard" />
             </div>
             <div className='btn__container'>
-                <Button variant="contained" className='btn' fontFamily="Ubuntu">Log in</Button>
+                <Button variant="contained" className='btn'>Log in</Button>
             </div>
             <div className='userAcc_container'>
-            <Typography variant="h8" gutterBottom fontFamily="Comfortaa" color="white" component="div">
-                Create new user account? <a href='#'>click here</a>
+            <Typography variant="h8" gutterBottom fontFamily="Ubuntu" color="#833471" component="div">
+                Create new user account? <a href='../User Register/UserRegister.js'>click here</a>
             </Typography>
             </div>     
         </div>  
