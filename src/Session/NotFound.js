bbd4d356b490d5c4702dcd79756819514224a3fb
oshadaera68 <mindartlk010@gmail.com>
2022-08-06 22:51:32 +0530

    updated content

--- src/Session/NotFound.js
+++ src/Session/NotFound.js
@@ -1,15 +1,17 @@
 import Button from '@mui/material/Button';
 import './NotFound.css'
 import notFound from './assets/404.webp'
+import {Link} from 'react-router-dom'
 function NotFound() {
     return (
         <div className='wrong__Container'>
             <div className='image__Container'>
                 <img src={notFound} alt='404 error' className='image' />
             </div>
-            <Button variant="contained" size="medium">
+            <Link to="/Login"><Button variant="outlined" size="medium">
                 Go To Back Page
             </Button>
+            </Link>
         </div>
     );
 }
