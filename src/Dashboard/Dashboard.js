3945ffe1855b3ec3c949d824d09c244edda08ef5
oshadaera68 <mindartlk010@gmail.com>
2022-08-09 21:02:31 +0530

    dashboard finished

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,5 +1,68 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import './Dashboard.css';
+
 function Dashboard() {
-   
+
+    return (
+        <div>
+            {/*Navigation Bar*/}
+            <header>
+                <nav className='nav-bar'>
+                    <div className='naming'>
+                        <p className='h3'>Dashboard</p>
+                    </div>
+                    <div className='btn__center'>
+                        <Button variant="contained" className='btn1' color='success'>Product</Button>
+                        <Button variant="contained" className='btn2' color='success'>Cart</Button>
+                    </div>
+                </nav>
+            </header>
+
+            {/*Cards*/}
+            <div>
+                <div className='card_1'>
+                    <Card sx={{ backgroundColor: 'red', height:'27vh'}}>
+                        <CardContent>
+                            <Typography variant="h4" className='type' gutterBottom component="div"
+                                        fontFamily='comfortaa'>
+                                <span className='span2' style={{top:'3vh', left:'2vh'}}>Products</span>
+                                <br/>
+                                <span className='span3' style={{top:'6vh', left:'9vh'}}>60</span>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className='card_2'>
+                    <Card sx={{backgroundColor: '#9b59b6', height:'27vh'}}>
+                        <CardContent>
+                            <Typography variant="h4" className='type1' gutterBottom component="div"
+                                        fontFamily='comfortaa, sans-serif'>
+                                <span className='span1' style={{top:'3vh', left:'6vh'}}>Cart</span>
+                                <br/>
+                                <span className='span' style={{top:'6vh', left:'9vh'}}>12</span>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className='card_3'>
+                    <Card sx={{backgroundColor: '#9b59b6', height:'27vh'}}>
+                        <CardContent>
+                            <Typography variant="h4" className='type1' gutterBottom component="div"
+                                        fontFamily='comfortaa, sans-serif'>
+                                <span className='span4' style={{top:'3vh', left:'6vh'}}>Users</span>
+                                <br/>
+                                <span className='span5' style={{top:'6vh', left:'9vh'}}>43</span>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Dashboard;
--- src/User Register/UserRegister.js
+++ src/User Register/UserRegister.js
@@ -2,7 +2,26 @@ import './UserRegister.css'
 import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import * as React from 'react';
+
+function createData(name, calories, fat, carbs, protein) {
+    return {name, calories, fat, carbs, protein};
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function UserRegister() {
     return (
@@ -11,28 +30,64 @@ function UserRegister() {
                 User Registration
             </Typography>
             <div className='text__container'>
-                <TextField id="standard-basic" label="First Name" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="Last Name" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="Email" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="User Name" variant="standard" className='txt' />
-                <TextField id="standard-password-input" label="Password" type="password" autoComplete="current-password" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="City" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="Street" variant="standard" className='' />
-                <TextField id="standard-basic" label="Street No" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="Zip Code" variant="standard" className='txt' />
-                <TextField id="standard-basic" label="Lat Value" variant="standard" className='lat__value' />
-                <TextField id="standard-basic" label="Long Value" variant="standard" className='long__value' />
-                <TextField id="standard-basic" label="Mobile No" variant="standard" className='mobile__no' />
+                <TextField id="standard-basic" label="First Name" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Last Name" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Email" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="User Name" variant="standard" className='txt'/>
+                <TextField id="standard-password-input" label="Password" type="password" autoComplete="current-password"
+                           variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="City" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Street" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Street No" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Zip Code" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Lat Value" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Long Value" variant="standard" className='txt'/>
+                <TextField id="standard-basic" label="Mobile No" variant="standard" className='txt'/>
             </div>
-            <div>
+            <div className='btn__container'>
                 <Button variant="contained" color='error' className='clear'>Clear</Button>
                 <Button variant="contained" color='success' className='save'>Save</Button>
             </div>
 
             <div>
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 650}} size="small" aria-label="a dense table" className='table'>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell classes='col'>First Name</TableCell>
+                                <TableCell classes='col'>Last Name</TableCell>
+                                <TableCell classes='col'>Email</TableCell>
+                                <TableCell classes='col'>City</TableCell>
+                                <TableCell classes='col'>Street</TableCell>
+                                <TableCell classes='col'>Street No</TableCell>
+                                <TableCell classes='col'>Zip Code</TableCell>
+                                <TableCell classes='col'>Lat Value</TableCell>
+                                <TableCell classes='col'>Long Value</TableCell>
+                                <TableCell classes='col'>Mobile No</TableCell>
+                                <TableCell classes='col'>Options</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {/*{rows.map((row) => (*/}
+                            {/*    <TableRow*/}
+                            {/*        key={row.name}*/}
+                            {/*        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}*/}
+                            {/*    >*/}
+                            {/*        <TableCell component="th" scope="row">*/}
+                            {/*            {row.name}*/}
+                            {/*        </TableCell>*/}
+                            {/*        <TableCell align="right">{row.calories}</TableCell>*/}
+                            {/*        <TableCell align="right">{row.fat}</TableCell>*/}
+                            {/*        <TableCell align="right">{row.carbs}</TableCell>*/}
+                            {/*        <TableCell align="right">{row.protein}</TableCell>*/}
+                            {/*    </TableRow>*/}
+                            {/*))}*/}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </div>
-    );
+);
 }
 
 export default UserRegister
